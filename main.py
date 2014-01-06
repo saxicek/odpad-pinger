4fcc234af3a97b0f925826a96052643c65638c9d
Libor Nenadál <libor.nenadal@gmail.com>
2014-01-06 21:41:49 +0100

    Initial version of http://odpad-praha8.rhcloud.com/status pinger

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 """ main.py is the top level script.
 
-Return "Hello World" at the root URL.
+Return response from http://odpad-praha8.rhcloud.com/status
 """
 
 import os
 import sys
+import json
+import urllib2
 
 # sys.path includes 'server/lib' due to appengine_config.py
 from flask import Flask
-from flask import render_template
+from flask import jsonify
 app = Flask(__name__.split('.')[0])
 
 
 @app.route('/')
-@app.route('/<name>')
-def hello(name=None):
-  """ Return hello template at application root URL."""
-  return render_template('hello.html', name=name)
-
-
+def ping():
+    """ Return hello template at application root URL."""
+    url = "http://odpad-praha8.rhcloud.com/status"
+    try:
+        result = urllib2.urlopen(url)
+        return jsonify(json.loads(result.read()))
+    except urllib2.URLError, e:
+        return jsonify({'error': e.message})
